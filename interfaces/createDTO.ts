284de6f114a2e3382072db0f7246e904caf749d4
Luke Bartholomew <lukesbart@icloud.com>
2023-03-04 17:49:43 -0500

    Create DTO for upload and create

---
+++ interfaces/createDTO.ts
@@ -0,0 +1,8 @@
+export type CreateDTO = {
+    event_title: string,
+    date: string,
+    description: string,
+    audio_url: string,
+    video_url: string,
+    handout_url: string
+}
---
+++ interfaces/uploadDTO.ts
@@ -0,0 +1,8 @@
+export type UploadDTO = {
+    title: string,
+    description: string,
+    date: string,
+    audio_url: string,
+    video_url: string,
+    handout_url: string
+};
