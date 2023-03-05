642a8713f3ca0fc04694af42e5d2b6bb19f358dd
Luke Bartholomew <lukesbart@icloud.com>
2023-03-04 21:14:07 -0500

    Add an updateDTO

---
+++ interfaces/updateDTO.ts
@@ -0,0 +1,9 @@
+export type UpdateDTO = {
+    id: number,
+    title: string;
+    description: string;
+    date: string;
+    audio_url: string,
+    video_url: string,
+    handout_url: string
+}
