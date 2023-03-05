8cb9e7f4bd8ac6a7d5a36f31042fd8e4953141e7
Luke Bartholomew <lukesbart@icloud.com>
2023-03-04 21:10:41 -0500

    Add CORS middlewars Add test authentication function Add test uuid and upload routes

--- index.js
+++ index.js
@@ -21,22 +21,43 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ dest: 'assets/', storage: storage });
+const cors = require('cors');
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
+app.use(cors());
 app.use("/assets", express_1.default.static('assets'));
 app.use("/public", express_1.default.static('public'));
+function authenticate(req, res, next) {
+    const auth = req.headers.authorization;
+    if (auth === 'secret') {
+        next();
+    }
+    else {
+        res.status(401);
+        res.send('Access forbidden');
+    }
+}
 const event = require("./routes/event.route");
 app.use("/event", event);
+const uploadtest = require("./routes/uploadtest.route");
+app.use("/upload", uploadtest);
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server');
 });
 app.get('/uuid', (req, res) => {
     res.send(uuidv4());
 });
-app.get('/uuid/:prefix/:ftype', (req, res) => {
-    res.send(`${req.params.prefix}-${uuidv4()}.${req.params.ftype}`);
+app.get('/uuid/:prefix', (req, res) => {
+    let guid = uuidv4();
+    ;
+    let fileNamesObj = {
+        mp3: `${req.params.prefix}-${guid}.mp3`,
+        mp4: `${req.params.prefix}-${guid}.mp4`,
+        pdf: `${req.params.prefix}-${guid}.pdf`,
+    };
+    res.json(fileNamesObj);
 });
-app.post('/pdf', upload.single('pdffile'), (req, res, next) => {
+app.post('/pdf', authenticate, upload.single('pdffile'), (req, res, next) => {
     res.send("File uploaded.");
 });
 app.listen(port, () => {
--- index.ts
+++ index.ts
@@ -20,15 +20,29 @@ const storage = multer.diskStorage({
   }
 })
 const upload = multer({ dest: 'assets/', storage: storage });
+const cors = require('cors');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 app.use("/assets", express.static('assets'));
 app.use("/public", express.static('public'));
 
+function authenticate(req: Request, res: Response, next: any) {
+  const auth = req.headers.authorization;
+  if (auth === 'secret') {
+    next();
+  } else {
+    res.status(401);
+    res.send('Access forbidden');
+  }
+}
 
 const event = require("./routes/event.route");
 app.use("/event", event);
+
+const uploadtest = require("./routes/uploadtest.route")
+app.use("/upload", uploadtest);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
@@ -38,11 +52,26 @@ app.get('/uuid', (req: Request, res: Response) => {
   res.send(uuidv4());
 });
 
-app.get('/uuid/:prefix/:ftype', (req: Request, res: Response) => {
-  res.send(`${req.params.prefix}-${uuidv4()}.${req.params.ftype}`)
+app.get('/uuid/:prefix', (req: Request, res: Response) => {
+  let guid = uuidv4();
+
+  interface fileNames {
+    mp3: string,
+    mp4: string,
+    pdf: string
+  };
+
+  let fileNamesObj: fileNames = {
+    mp3: `${req.params.prefix}-${guid}.mp3`,
+    mp4: `${req.params.prefix}-${guid}.mp4`,
+    pdf: `${req.params.prefix}-${guid}.pdf`,
+  }
+
+
+  res.json(fileNamesObj);
 });
 
-app.post('/pdf', upload.single('pdffile'), (req: Request, res: Response, next: any) => {
+app.post('/pdf', authenticate, upload.single('pdffile'), (req: Request, res: Response, next: any) => {
   res.send("File uploaded.");
 });
 
