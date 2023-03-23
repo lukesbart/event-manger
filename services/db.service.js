93d9e9c4897d8a5b389b807445ffb54487752909
Luke Bartholomew <lukesbart@icloud.com>
2023-03-23 19:13:39 -0400

    Remove js outside of dist folder

--- services/db.service.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient();
-const event = prisma.event;
-module.exports = { prisma, event };
